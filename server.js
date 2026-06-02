d211ed3d9f8864c572168c086b61b4b2a23f3c7e
TOURE Mamadou <mamdouaboubakary572@gmail.com>
2026-06-02 23:47:06 +0000

    fix port binding

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`BIOSAVEUR server running on port ${port}`);
 });
